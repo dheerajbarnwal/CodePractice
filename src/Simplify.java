2ec313799fb51bbd125a1b7cd8b3985490c78ee1
dheerajraaj <dheerajraaj2@gmail.com>
2018-06-11 00:41:55 +0530

    Comment added for better understanding

--- src/Simplify.java
+++ src/Simplify.java
@@ -11,27 +11,36 @@ public class Simplify {
         int prev;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='('){
-                prev=i-1;       //check previous character
+                /*check operators just before starting of bracket means '('.
+                 If operator is -, we need to toggle all operators inside the bracket
+                 So, here I have used 0 and 1 in stack to indicate if an operator comes inside this bracket need to toggle or not in the result.
+                 0 : Not toggle
+                 1 : toggle
+
+                 stack.push when character '('
+                 stack.pop when character ')'   */
+
+                prev=i-1;
                 if(prev >= 0){
                     if(s.charAt(prev)=='-'){
                         if(!st.isEmpty() && st.peek()==1)
                             st.push(0);
                         else
                             st.push(1);
-                    }else{  // if previous character is '+' or '('
+                    }else{                      // if previous character is '+' or '('
                         if(!st.isEmpty() && st.peek()==1)
                             st.push(1);
                         else
                             st.push(0);
                     }
-                }else{
+                }else{                   //it will handle only the case when string starts with '('
                     st.push(0);
                 }
             }else if(Character.isLetter(s.charAt(i))){
                 res+=s.charAt(i);
             }else if(s.charAt(i)==')'){
                 st.pop();
-            }else if(!st.isEmpty() && st.peek()==1){
+            }else if(!st.isEmpty() && st.peek()==1){  // Toggle here on the basis of top of stack
                 if(s.charAt(i)=='-'){
                     res+='+';
                 }else{
