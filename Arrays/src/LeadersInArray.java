efffbb164a008f5855296054a66c8635b7cd9406
dheerajraaj <dheerajraaj2@gmail.com>
2018-07-31 20:38:47 +0530

    Leaders in an array

---
+++ Arrays/src/LeadersInArray.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Leaders in an array
+ * print all the LEADERS in the array. An element is leader if it is greater than all the elements to its right side. The rightmost element is always a leader.
+ */
+public class LeadersInArray {
+    private static Stack maxSumInc(int a[],int n){
+        Stack<Integer> st = new Stack<>();
+        int currLead = -1;
+        for(int i=n-1;i>=0;i--){
+            if(a[i] > currLead){
+                st.push(a[i]);
+                currLead = a[i];
+            }
+        }
+        return st;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int a[],n;
+        Stack<Integer> ans;
+        for(int i=0;i<t;i++){
+            n = sc.nextInt();
+            a = new int[n];
+            for(int j=0;j<n;j++){
+                a[j] = sc.nextInt();
+            }
+            ans = maxSumInc(a,n);
+            while(!ans.isEmpty()) {
+                System.out.print(ans.pop() + " ");
+            }
+            System.out.println();
+        }
+    }
+}
