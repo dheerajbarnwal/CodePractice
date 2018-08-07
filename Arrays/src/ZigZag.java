02bab3c08ea1410990b05b38623dd6d7ec7c40f2
dheerajraaj <dheerajraaj2@gmail.com>
2018-08-07 23:29:12 +0530

    Convert array into Zig-Zag fashion

--- Arrays/src/ZigZag.java
+++ Arrays/src/ZigZag.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
  * Given an array of distinct elements, rearrange the elements of array in zig-zag fashion in O(n) time. The converted array should be in form a < b > c < d > e < f.The relative order of elements is same in the output i.e you have to iterate on the original array only.
  */
 public class ZigZag {
+    private static void swap(int a[],int i){
+        int temp = a[i];
+        a[i] = a[i + 1];
+        a[i + 1] = temp;
+    }
     private static void createZigZag(int a[],int n){
         boolean flag =false;
         for(int i=0;i<n-1;i++) {
             if(flag == true && a[i]<a[i+1]) {
-                int temp = a[i];
-                a[i] = a[i + 1];
-                a[i + 1] = temp;
+                swap(a,i);
             }
             else if(flag == false && a[i]>a[i+1]) {
-                int temp = a[i];
-                a[i] = a[i + 1];
-                a[i + 1] = temp;
+                swap(a,i);
             }
             flag = !flag;
         }
