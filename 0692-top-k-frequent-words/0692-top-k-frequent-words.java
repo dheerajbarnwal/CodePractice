4f0e7b513185de91b7a4f68e81b94c8cf64a3304
Dheeraj Kumar Barnwal <dheerajraaj2@gmail.com>
2022-10-19 20:42:36 +0530

    Time: 10 ms (75.40%), Space: 45.4 MB (42.39%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,7 +1,7 @@
 class Solution {
     
     public List<String> topKFrequent(String[] words, int k) {
-        PriorityQueue<WordFreq> pq = new PriorityQueue(k, new WordComparator());
+        PriorityQueue<Map.Entry<String,Integer>> pq = new PriorityQueue(k, new WordComparator());
         Map<String,Integer> hm = new HashMap<>();
         
         for(int i=0;i<words.length;i++){
@@ -9,43 +9,28 @@ class Solution {
         }
         
         for(Map.Entry<String,Integer> entry: hm.entrySet()){
-            WordFreq wf1 = new WordFreq(entry.getKey(), entry.getValue());
-            //System.out.println(entry.getKey() + " " + entry.getValue());
             if(pq.size()==k){
-                //System.out.println(pq.peek().word);
-                if(entry.getValue()<pq.peek().count){
-                    continue;
-                }else if(entry.getValue()==pq.peek().count && entry.getKey().compareTo(pq.peek().word)>0){
-                    //System.out.println(entry.getKey());
+                if(entry.getValue()<pq.peek().getValue() || (entry.getValue()==pq.peek().getValue() && entry.getKey().compareTo(pq.peek().getKey())>0)){
                     continue;
                 }
                 pq.poll();
             }
-            pq.add(wf1);
+            pq.add(entry);
         }
         List<String> ans = new ArrayList<>();
         while(!pq.isEmpty()){
-            ans.add(pq.poll().word);
+            ans.add(0,pq.poll().getKey());
         }
-        Collections.reverse(ans);
         return ans;
     }
 }
 
-class WordFreq{
-        String word;
-        int count;
-        WordFreq(String word, int count){
-            this.word = word;
-            this.count = count;
+class WordComparator implements Comparator<Map.Entry<String,Integer>>{
+    public int compare(Map.Entry<String,Integer> w1, Map.Entry<String,Integer> w2){
+        if(w1.getValue()==w2.getValue()){
+            return w2.getKey().compareTo(w1.getKey());
         }
-    }
-class WordComparator implements Comparator<WordFreq>{
-    public int compare(WordFreq w1, WordFreq w2){
-        if(w1.count==w2.count){
-            return w2.word.compareTo(w1.word);
-        }
-        return w1.count-w2.count;
+        return w1.getValue()-w2.getValue();
     }
 }
 
