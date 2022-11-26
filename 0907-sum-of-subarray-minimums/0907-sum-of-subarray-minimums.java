a419808c1800e3733408d761573b314332202437
Dheeraj Kumar Barnwal <dheerajraaj2@gmail.com>
2022-11-26 13:56:10 +0530

    Time: 124 ms (42.30%), Space: 50.1 MB (84.32%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,38 +1,40 @@
-
 class Solution {
     public int sumSubarrayMins(int[] arr) {
         int MOD = 1000000007;
-
-        Stack<Integer> stack = new Stack<>();
-        long sumOfMinimums = 0;
-
-        // building monotonically increasing stack
-        for (int i = 0; i <= arr.length; i++) {
-
-            // when i reaches the array length, it is an indication that
-            // all the elements have been processed, and the remaining
-            // elements in the stack should now be popped out.
-
-            while (!stack.empty() && (i == arr.length || arr[stack.peek()] >= arr[i])) {
-
-                // Notice the sign ">=", This ensures that no contribution
-                // is counted twice. rightBoundary takes equal or smaller 
-                // elements into account while leftBoundary takes only the
-                // strictly smaller elements into account
-
-                int mid = stack.pop();
-                int leftBoundary = stack.empty() ? -1 : stack.peek();
-                int rightBoundary = i;
-
-                // count of subarrays where mid is the minimum element
-                long count = (mid - leftBoundary) * (rightBoundary - mid) % MOD;
-
-                sumOfMinimums += (count * arr[mid]) % MOD;
-                sumOfMinimums %= MOD;
+        Stack<Integer> st = new Stack<>();
+        int n=arr.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        
+        for(int i=0;i<n;i++){
+            int temp=1;
+            while(!st.isEmpty() &&arr[i]<=arr[st.peek()]){
+                temp+=left[st.pop()];
             }
-            stack.push(i);
+            left[i] = temp;
+            st.push(i);
         }
-
-        return (int) (sumOfMinimums);
+        
+        st.clear();
+        
+        
+        for(int i=n-1;i>=0;i--){
+            int temp=1;
+            while(!st.isEmpty() &&arr[i]<arr[st.peek()]){
+                temp+=right[st.pop()];
+            }
+            right[i] = temp;
+            st.push(i);
+        }
+        
+        int ans=0;
+        
+        for(int i=0;i<n;i++){
+            // int temp = (((left[i]+right[i]-1)*(left[i]+right[i]))/2) - (((left[i]-1)*left[i])/2) - (((right[i]-1)*right[i])/2);
+            long temp = left[i]*right[i];
+            ans+= (temp*arr[i])%MOD;
+            ans%=MOD;
+        }
+        return ans;
     }
 }
