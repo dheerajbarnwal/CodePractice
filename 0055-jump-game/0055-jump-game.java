ee3787c32fc03826988ff9d608e9606aad4ef025
Dheeraj Kumar Barnwal <dheerajraaj2@gmail.com>
2022-12-26 14:21:06 +0530

    Time: 2 ms (92.42%), Space: 42.8 MB (85.72%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,21 +1,38 @@
 class Solution {
+
     public boolean canJump(int[] nums) {
+        //return canJump1(nums);
+        return canJump2(nums);
+    }
+
+    boolean canJump1(int[] nums) {
         int n = nums.length;
-        int target=n-1;
-        
-        while(target!=0){
-            int start=target-1;
-            while(start>=0 && start+nums[start]<target){
+        int target = n - 1;
+
+        while (target != 0) {
+            int start = target - 1;
+            while (start >= 0 && start + nums[start] < target) {
                 start--;
             }
-            
-            if(start<0 && target!=0){
+
+            if (start < 0 && target != 0) {
                 return false;
             }
-            
-            target=start;
-            
+
+            target = start;
         }
         return true;
+    }
+
+    boolean canJump2(int[] nums) {
+        int n = nums.length;
+        int reach=0;
+        for(int i=0;i<=reach;i++){
+            reach=Math.max(reach,i+nums[i]);
+            if(reach>=n-1){
+                return true;
+            }
+        }
+        return false;
     }
 }
